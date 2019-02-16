15e155787a7cfb65cfe6f774a2f5d0bc87531e84
deyou <1157826511@qq.com>
2019-02-16 10:23:28 +0800

    add helloworld testweb

---
+++ mvn/hello-world/src/main/java/com/zhaideyou/mvnbook/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.zhaideyou.mvnbook.helloworld;
+public class HelloWorld{
+	public String  sayHello(){
+		return "Hello Maven";
+	}
+	public static void main(String [] args){
+		System.out.println(new HelloWorld().sayHello());
+	}
+}
